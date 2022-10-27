22d26a5ab59d4c3951af037a6c30b8a8dd457f95
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-10-27 22:59:08 +0100

    Improved typing

---
+++ mediate/middleware.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass, field
+import functools
+from typing import Callable, Generic, Protocol, TypeVar
+from typing_extensions import ParamSpec
+
+from roster import Record
+
+PS = ParamSpec("PS")
+RT = TypeVar("RT")
+
+
+class MiddlewareCallable(Protocol[PS, RT]):
+    def __call__(
+        self, call_next: Callable[PS, RT], *args: PS.args, **kwargs: PS.kwargs
+    ) -> RT:
+        ...
+
+
+@dataclass
+class PartialMiddlewareCallable(Generic[PS, RT]):
+    middleware: MiddlewareCallable[PS, RT]
+    call_next: Callable[PS, RT]
+
+    def __call__(self, *args: PS.args, **kwargs: PS.kwargs) -> RT:
+        return self.middleware(self.call_next, *args, **kwargs)
+
+
+@dataclass
+class Middleware(Generic[PS, RT]):
+    record: Record[MiddlewareCallable[PS, RT]] = field(default_factory=Record)
+
+    def __call__(
+        self, middleware: MiddlewareCallable[PS, RT], /
+    ) -> MiddlewareCallable[PS, RT]:
+        return self.record(middleware)
+
+    def compose(self, sinc: Callable[PS, RT], /) -> Callable[PS, RT]:
+        call_next: Callable[PS, RT] = sinc
+
+        middleware: MiddlewareCallable[PS, RT]
+        for middleware in self.record:
+            call_next = PartialMiddlewareCallable(middleware, call_next)
+
+        return call_next
+
+    def bind(self, sinc: Callable[PS, RT], /) -> Callable[PS, RT]:
+        @functools.wraps(sinc)
+        def wrapper(*args: PS.args, **kwargs: PS.kwargs):
+            return self.compose(sinc)(*args, **kwargs)
+
+        return wrapper
